cb673c3812a46b9a88636a7db3cfb03b0c2492ef
shuang.wang <shuang.wang@51zouchuqu.com>
2019-09-20 03:00:23 +0800

    add route

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import  { Hero }  from '../hero';
-import { HEROES } from '../mock-heroes';
+import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,15 +10,19 @@ import { HEROES } from '../mock-heroes';
 
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: "Windstorm",
-  };
-  heroes = HEROES
+  heroes: Hero[]
 
-  constructor() { }
+  constructor(private heroService: HeroService) {
+  
+  }
 
   ngOnInit() {
+    this.getHeros()
+  }
+
+
+  getHeros(): void {
+    this.heroService.getHeros().subscribe(heros => this.heroes = heros)
   }
 
 }
